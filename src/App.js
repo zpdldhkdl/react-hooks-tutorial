4606dcce57aaa6a4732fbdb5ffbf93b078ecfd4b
DESKTOP-A43AVN4\kyong <lklklnm@daum.net>
2020-11-19 16:46:23 +0900

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import UseState from "./components/UseState";
 import UseEffect from "./components/UseEffect";
+import UseReducer from "./components/UseReducer";
+import UseRef from "./components/UseRef";
+import './App.css';
 
 export default function App() {
     return (
-        <div>
+        <div className="main">
             <UseState />
             <UseEffect />
+            <UseReducer />
+            <UseRef />
         </div>
     )
 }
